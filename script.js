c889b965292eefdfe71c3b08b51846da71d37f7c
Isabela Costa <belagames012@gmail.com>
2024-03-27 13:56:28 -0300

    quarto commit do java

--- script.js
+++ script.js
@@ -30,15 +30,24 @@ function ReceberQuantidadePessoas(evento) {
 
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", ReceberPorcentagemBotao)
+    botao.addEventListener("click", ReceberPorcentagem)
 })
 
-function ReceberPorcentagemBotao(evento) {
+function ReceberPorcentagem(evento)  {
     botoesGorjeta.forEach(botao=> {
         botao.classList.remove("botao-ativo")
         if(botao.value === evento.target.value) {
             botao.classList.add("botao-ativo")
         }
     })
-    porcentagem = parseFloat(evento.target.value) / 100
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value) / 100
+    }
+    else {
+        porcentagem =0
+    }
+    console.log(porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", ReceberPorcentagem)
